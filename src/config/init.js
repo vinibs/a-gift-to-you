5803101e4f1d4a66c62afd854bb80a3b766647c8
Vinicius Baroni Soares <hiviniciusbs@gmail.com>
2024-12-15 13:21:30 -0300

    refactor: move main imports to init file

--- src/config/init.js
+++ src/config/init.js
@@ -1,1 +1,2 @@
 import '../components/index.js'
+import '../views/index.js'
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,8 +1,6 @@
 // Imports the route instance to set the App's routes
 import { route } from '../../js/lira.js'
-
-import '../components/index.js'
-import '../views/index.js'
+import '../config/init.js'
 
 route.set('/', 'app-index')
 
